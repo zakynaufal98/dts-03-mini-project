289ee2f8493b3d54f1b51b278f11dd2994cee3e0
Yazid Yasykur <yasykury@gmail.com>
2022-07-18 20:16:23 +0700

    Fixed Navbar responsive

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,18 +1,13 @@
 
 import logo from "../assets/logo.png"
-import vectorSearch from "../assets/search.png"
-import vectorNotif from "../assets/notif.png"
-import vectorGiftBox from "../assets/gift-box.png"
-import vectorDropdown from "../assets/drop-down.png"
-import profileImage from "../assets/profile-img.png"
 
-import {GiftIcon, SearchIcon} from "@heroicons/react/outline"
-import {BellIcon, UserIcon, ChevronDoubleDownIcon} from "@heroicons/react/solid"
+import { GiftIcon, SearchIcon } from "@heroicons/react/outline"
+import { BellIcon, UserIcon, ChevronDoubleDownIcon } from "@heroicons/react/solid"
 
 const Navbar = () => {
 
   return (
-    <div className="Header container:xs pr-4 flex flex-row h-16 w-full items-center bg-black justify-between">
+    <div className="Header container:sm pr-6 flex flex-row h-16 w-full items-center bg-black justify-between">
       <div className="HeaderLeft flex gap-8 ml-[60px]">
         {/* Logo Image */}
         <img src={logo} alt="logo" className="Logo w-[36px] h-[44px]" />
@@ -29,16 +24,16 @@ const Navbar = () => {
       </div>
 
       {/* Menu Kanan */}
-      <div className="HeaderRight flex justify-self-end">
+      <div className="HeaderRight flex justify-self-end invisible sm:visible">
+        <SearchIcon className="text-white w-6 h-6 visible absolute right-3 sm:relative" />
         <ul className="flex gap-5 items-center text-white text-xs">
-          <li> <SearchIcon className="w-6 h-6"/></li>
           <li> User </li>
-          <li> <BellIcon className="w-6 h-6"/></li>
-          <li> <GiftIcon className="w-6 h-6"/> </li>
+          <li> <BellIcon className="w-6 h-6" /></li>
+          <li> <GiftIcon className="w-6 h-6" /> </li>
           <div className="flex items-center gap-[10px]">
-            <li><UserIcon className="w-6 h-6"/></li>
-            <li><ChevronDoubleDownIcon className="w-6 h-6"/></li>
-          </div> 
+            <li><UserIcon className="w-6 h-6" /></li>
+            <li><ChevronDoubleDownIcon className="w-6 h-6" /></li>
+          </div>
         </ul>
       </div>
     </div>
