08de71c25b8fe1ea2df7b296ac5488cedac4f8cc
Yazid Yasykur <yasykury@gmail.com>
2022-07-18 19:43:18 +0700

    Changed navbar icons and fixed responsive for mobile

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,4 +1,4 @@
-import react, { useState } from "react";
+
 import logo from "../assets/logo.png"
 import vectorSearch from "../assets/search.png"
 import vectorNotif from "../assets/notif.png"
@@ -6,15 +6,18 @@ import vectorGiftBox from "../assets/gift-box.png"
 import vectorDropdown from "../assets/drop-down.png"
 import profileImage from "../assets/profile-img.png"
 
+import {GiftIcon, SearchIcon} from "@heroicons/react/outline"
+import {BellIcon, UserIcon, ChevronDoubleDownIcon} from "@heroicons/react/solid"
+
 const Navbar = () => {
 
   return (
-    <div className="Header container flex flex-row h-16 w-full items-center bg-black justify-between">
+    <div className="Header container:xs pr-4 flex flex-row h-16 w-full items-center bg-black justify-between">
       <div className="HeaderLeft flex gap-8 ml-[60px]">
         {/* Logo Image */}
         <img src={logo} alt="logo" className="Logo w-[36px] h-[44px]" />
         {/* Menu Kiri */}
-        <div className="MenuItems flex">
+        <div className="MenuItems flex invisible md:visible ">
           <ul className="flex flex-row gap-[20px] text-white text-xs items-center ">
             <li>Home</li>
             <li>Series</li>
@@ -28,13 +31,13 @@ const Navbar = () => {
       {/* Menu Kanan */}
       <div className="HeaderRight flex justify-self-end">
         <ul className="flex gap-5 items-center text-white text-xs">
-          <li> <img src={vectorSearch} alt="search"/></li>
+          <li> <SearchIcon className="w-6 h-6"/></li>
           <li> User </li>
-          <li> <img src={vectorNotif} alt="notify"/></li>
-          <li> <img src={vectorGiftBox} alt="box"/></li>
+          <li> <BellIcon className="w-6 h-6"/></li>
+          <li> <GiftIcon className="w-6 h-6"/> </li>
           <div className="flex items-center gap-[10px]">
-            <li><img src={profileImage} alt="" /></li>
-            <li><img src={vectorDropdown} alt="" /></li>
+            <li><UserIcon className="w-6 h-6"/></li>
+            <li><ChevronDoubleDownIcon className="w-6 h-6"/></li>
           </div> 
         </ul>
       </div>
