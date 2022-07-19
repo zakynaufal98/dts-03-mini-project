41909b190ddf5f7b94e2f6bc5bd7d57ab6e3ecf1
zakynaufal98 <zakynaufal1998@gmail.com>
2022-07-19 14:32:15 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import LoginForm from "./Components/LoginForm";
 const App = () => {
   return (
     <div className="w-full overflow-x-hidden">
-      <Navbar />
-<<<<<<< HEAD
-      <Footer />
-=======
       <LoginForm />
->>>>>>> c4431d27ec595fa1933c0cc81eb8a31dab55b303
     </div>
   );
 };
